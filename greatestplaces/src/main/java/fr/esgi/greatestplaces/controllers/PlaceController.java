f79738e7da07115a5345c2c607446e8d7aac4185
aragant <tlemaire1610@gmail.com>
2022-07-09 19:58:46 +0200

    getPlacesById added

--- greatestplaces/src/main/java/fr/esgi/greatestplaces/controllers/PlaceController.java
+++ greatestplaces/src/main/java/fr/esgi/greatestplaces/controllers/PlaceController.java
@@ -10,8 +10,13 @@ import fr.esgi.greatestplaces.entities.Place;
 import fr.esgi.greatestplaces.entities.User;
 import fr.esgi.greatestplaces.repositories.PlaceRepository;
 import fr.esgi.greatestplaces.services.AuthService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -38,5 +43,10 @@ public class PlaceController {
         place.setUserId(currentUser.getId());
         return this.placeRepository.save(place);
     }
-    
+
+
+    @GetMapping(value="/{userId}")
+    public List<Place> getPlacesByUserId(@PathVariable Long userId) {
+        return this.placeRepository.findAllByUserId(userId);
+    }
 }
--- greatestplaces/src/main/java/fr/esgi/greatestplaces/repositories/PlaceRepository.java
+++ greatestplaces/src/main/java/fr/esgi/greatestplaces/repositories/PlaceRepository.java
@@ -1,10 +1,19 @@
 package fr.esgi.greatestplaces.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import fr.esgi.greatestplaces.entities.Place;
-import java.util.Collection;
 
 public interface PlaceRepository extends JpaRepository<Place, Long> {
+    Place getPostById(Long id);
+    
+    List<Place> findAllByName(String name);
+
+    List<Place> findAllByUserId(Long userId);
+
+    List<Place> findAllByNameLike(String name);
+
+    List<Place> findAllByUserIdAndNameLikeIgnoreCase(Long userId, String name);
 }
